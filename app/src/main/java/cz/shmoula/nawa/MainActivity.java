cbee3f9b41647ff014882c9b3ba7a0539fcebebd
shmoula <shmoula@gmail.com>
2015-08-01 21:30:26 +0200

    Filtering assets by name

--- app/src/main/java/cz/shmoula/nawa/MainActivity.java
+++ app/src/main/java/cz/shmoula/nawa/MainActivity.java
@@ -2,9 +2,11 @@ package cz.shmoula.nawa;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,6 +19,8 @@ import cz.shmoula.nawa.util.DataLoader;
  * Created by vbalak on 01/08/15.
  */
 public class MainActivity extends AppCompatActivity {
+    private AssetAdapter assetAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        AssetAdapter assetAdapter = new AssetAdapter();
+        assetAdapter = new AssetAdapter();
         recyclerView.setAdapter(assetAdapter);
 
         DataLoader dataLoader = new DataLoader(this, assetAdapter);
@@ -35,6 +39,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem filterItemItem = menu.findItem(R.id.action_filter);
+        SearchView filterView = (SearchView) MenuItemCompat.getActionView(filterItemItem);
+        filterView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                assetAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+
         return true;
     }
 
--- app/src/main/java/cz/shmoula/nawa/adapter/AssetAdapter.java
+++ app/src/main/java/cz/shmoula/nawa/adapter/AssetAdapter.java
@@ -4,7 +4,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cz.shmoula.nawa.R;
@@ -13,10 +16,12 @@ import cz.shmoula.nawa.view.AssetViewHolder;
 
 /**
  * Data holder for list of assets
+ * Provides simple filtering capabilities - by asset name
  * Created by vbalak on 01/08/15.
  */
-public class AssetAdapter extends RecyclerView.Adapter<AssetViewHolder> {
-    private List<Asset> data;
+public class AssetAdapter extends RecyclerView.Adapter<AssetViewHolder> implements Filterable {
+    private List<Asset> allAssets;
+    private List<Asset> filteredAssets;
 
     @Override
     public AssetViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
@@ -27,16 +32,63 @@ public class AssetAdapter extends RecyclerView.Adapter<AssetViewHolder> {
 
     @Override
     public void onBindViewHolder(AssetViewHolder assetViewHolder, int i) {
-        assetViewHolder.setAsset(data.get(i));
+        assetViewHolder.setAsset(filteredAssets.get(i));
     }
 
     @Override
     public int getItemCount() {
-        return data == null ? 0 : data.size();
+        return filteredAssets == null ? 0 : filteredAssets.size();
     }
 
+    /**
+     * Sets ALL data for this adapter
+     */
     public void setData(List<Asset> data) {
-        this.data = data;
+        allAssets = data;
+        filteredAssets = data;
         notifyDataSetChanged();
+    }
+
+    /**
+     * Sets filtered data - everything visible in the list
+     */
+    private void setFilteredAssets(List<Asset> data) {
+        filteredAssets = data;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                if(allAssets == null || allAssets.size() < 1)
+                    return null;
+
+                String filterString = charSequence.toString().toLowerCase();
+                List<Asset> filteredList = new ArrayList<Asset>();
+
+                for(Asset asset : allAssets) {
+                    String assetName = asset.getName().toLowerCase();
+                    if(assetName.contains(filterString))
+                        filteredList.add(asset);
+                }
+
+                FilterResults results = new FilterResults();
+                results.values = filteredList;
+                results.count = filteredList.size();
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                if(filterResults == null || filterResults.count < 1)
+                    return;
+
+                setFilteredAssets((List<Asset>) filterResults.values);
+            }
+        };
     }
 }
