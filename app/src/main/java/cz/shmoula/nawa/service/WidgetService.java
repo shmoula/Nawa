3e844fb7172fc30604bdda6ff1869e08feb35705
shmoula <shmoula@gmail.com>
2015-08-02 13:16:55 +0200

    Simple widget stub - holds textviews in list, which shows watched assets

---
+++ app/src/main/java/cz/shmoula/nawa/service/WidgetService.java
@@ -0,0 +1,7 @@
+package cz.shmoula.nawa.service;
+
+/**
+ * Created by vbalak on 02/08/15.
+ */
+public class WidgetService {
+}
---
+++ app/src/main/java/cz/shmoula/nawa/view/WidgetViewsFactory.java
@@ -0,0 +1,7 @@
+package cz.shmoula.nawa.view;
+
+/**
+ * Created by vbalak on 02/08/15.
+ */
+public class WidgetViewsFactory {
+}
